701ca4f4df6f3b0dfde481352177b92ac559d7c6
Max Mashanov <ultramax765@vk.com>
2024-06-15 23:37:48 +0800

    Добавление отчета ПП01

--- Desktop/intStore/View/MainWindow.xaml.cs
+++ Desktop/intStore/View/MainWindow.xaml.cs
@@ -56,46 +56,12 @@ namespace intStore.View
                 return;
             }
             
-            if(!IsValidUserName(username))
-            {
-                MessageBox.Show("Некорректное имя. Пример: Max");
-                return;
-            }
-
-            if (!IsValidEmail(email))
-            {
-                MessageBox.Show("Некорректный email. Пример: max@gmail.com");
-                return;
-            }
-
-            if (!IsValidPassword(password))
-            {
-                MessageBox.Show("Некорректный пароль. Пример: *Mm12311");
-                return;
-            }
-
             bool registrationSuccessful = authService.Register(username, email, password);
             if (registrationSuccessful)
             {
                 MessageBox.Show("Регистрация прошла успешно!");
                 ClearTextBox();
             }
-        }
-
-        private bool IsValidUserName(string username)
-        {
-            string pattern = @"^\S+$";
-            return Regex.IsMatch(username, pattern);
-        }
-        private bool IsValidEmail(string email)
-        {
-            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$"; 
-            return Regex.IsMatch(email, pattern);
-        }
-        private bool IsValidPassword(string password)
-        {
-            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"; 
-            return Regex.IsMatch(password, pattern);
         }
 
         private void ClearTextBox()
