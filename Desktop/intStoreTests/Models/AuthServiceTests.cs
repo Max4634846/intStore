1c6016ac587d5ecd96b65d887104788c243dca2d
Max Mashanov <ultramax765@vk.com>
2024-06-18 16:54:26 +0800

    Word 3

---
+++ Desktop/intStoreTests/Models/AuthServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using intStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intStore.Models.Tests
+{
+    [TestClass()]
+    public class AuthServiceTests
+    {
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            AuthService authService = new AuthService();
+            //Предусловие 
+            string name = "Elena";
+            string email = "eleds2345@gmail.com";
+            string password = "*Mm1234511";
+            bool expected = true;
+
+            //Действие 
+            bool actual = authService.Register(name, email, password);
+
+            //Утверждение 
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
